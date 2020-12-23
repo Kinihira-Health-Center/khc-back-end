483d9973ad9df64e2647cb26245552d0258b9d22
Igor Jean Luc Ndiramiye <nigorjeanluc@gmail.com>
2020-12-23 06:47:32 +0200

    model creation

--- server/models/user.js
+++ server/models/user.js
@@ -31,21 +31,6 @@ const userDefinition = (sequelize, DataTypes) => {
       as: 'token',
       onDelete: 'CASCADE',
     });
-    User.hasMany(models.contact, {
-      foreignKey: 'userId',
-      as: 'contacts',
-      onDelete: 'CASCADE',
-    });
-    User.hasMany(models.comment, {
-      foreignKey: 'user_id',
-      as: 'comments',
-      onDelete: 'CASCADE',
-    });
-    User.hasMany(models.view, {
-      foreignKey: 'user_id',
-      as: 'views',
-      onDelete: 'CASCADE',
-    });
   };
   return User;
 };
